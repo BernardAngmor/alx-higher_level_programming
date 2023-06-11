6a8196125fe39544c5ea18fbb537076887fabb8e
BernardAngmor <bernardangmor98@gmail.com>
2023-06-11 11:50:32 -0700

    Trying something new

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    """
+    Prints all integers of a list.
+
+    Args:
+        my_list (list): List of integers.
+
+    Returns:
+        None
+    """
+    for num in my_list:
+        print("{0}".format(num))
+
